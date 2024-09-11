39739c7a7e78d0033544596e6c80ed4395680efc
SmarkWar <miguelceballos2000@gmail.com>
2024-09-10 21:24:42 -0600

    Agregue los archivos de la Tarea #5 de la Unidad 2

---
+++ main.py
@@ -0,0 +1,12 @@
+from circulo import Circulo
+
+print("------------------------------------------------------------")
+circulo1 = Circulo(3)
+print("Círculo 1 - Radio", circulo1.radio, "Área", circulo1.calcular_area(), "Perímetro", circulo1.calcular_perimetro())
+print("------------------------------------------------------------")
+circulo2 = Circulo(5)
+print("Círculo 2 - Radio", circulo2.radio, "Área", circulo2.calcular_area(), "Perímetro", circulo2.calcular_perimetro())
+print("------------------------------------------------------------")
+circulo3 = Circulo(7)
+print("Círculo 3 - Radio", circulo3.radio, "Área", circulo3.calcular_area(), "Perímetro", circulo3.calcular_perimetro())
+print("------------------------------------------------------------")
