0421e48334cf82f417e5ca18eb33f013cc1c2a63
SmarkWar <miguelceballos2000@gmail.com>
2024-09-17 23:00:11 -0600

    Ejercicio en Proceso

---
+++ medico/medico.py
@@ -0,0 +1,37 @@
+
+import random
+
+class Medico:
+    id: int
+    nombre: str
+    nacimiento: int
+    rfc: str
+    direccion: str
+    
+    def __init__(self, nombre: str, nacimiento: int, rfc: str, direccion: str):
+        self.id = random.randint(1, 100000)
+        self.nombre = nombre
+        self.nacimiento = nacimiento
+        self.rfc = rfc
+        self.direccion = direccion
+    
+@property
+def id(self):
+    return self.id
+
+@property
+def nombre(self):
+    return self.nombre
+
+@property
+def nacimiento(self):
+    return self.nacimiento
+
+@property
+def rfc(self):
+    return self.rfc
+
+@property
+def direccion(self):
+    return self.direccion
+ 
