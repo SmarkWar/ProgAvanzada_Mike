a118decfdcf70670b13a64fae460f06e02a923d0
SmarkWar <miguelceballos2000@gmail.com>
2024-11-19 14:46:32 -0600

    No Se

---
+++ login-2.py
@@ -0,0 +1,14 @@
+
+"""
+select (nombre, correo, contra) from <nombre de la tabla>
+select * from <nombre de la tabla> where correo = " " and contra = " "    selecciona todos los datos de la tabla
+select {nombre} from <nombre de la tabla> where id = 2
+
+insert into <nombre de la tabla> (nombre, correo, contra) values (" ", " ", " ")
+
+update <nombre de la tabla> set contra == " " where id = 2      editar algo
+
+delete from <nombre de la tabla>      borra toda la base de datos
+
+delete from <nombre de la tabla> where id = <id>
+"""
